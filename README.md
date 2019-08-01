d4d15205ae9d295ba2d5ef1ab838bc7a2ec5c350
NekuSakuraba <nekom3m3>
2019-08-01 19:50:09 -0300

    Documenting bandits and correcting some typos

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# reinforcement-learning
+# Table of Contents
+- [01 - Multi-armed bandit problem](/01%20-%20Multi-armed%20bandit%20problem.ipynb)
--- utils/bandit.py
+++ utils/bandit.py
@@ -2,19 +2,40 @@ import numpy as np
 
 
 class Bandit:
+    """One-armed bandit"""
     
     def __init__(self, prob):
+        """
+        Parameters
+        ----------
+        prob : float
+            probability of the jackpot
+        """
         self.p = prob
     
     def pull(self):
+        """Pull the lever of the bandit"""
         return 1 if np.random.random() < self.p else 0
 
     
 class Bandits:
+    """n-armed bandit"""
     
     def __init__(self, probs):
+        """
+        Parameters
+        ----------
+        probs : list
+            list of probabilities of each bandit to jackpot
+        """
         self.n = len(probs)
         self.bandits = [Bandit(p) for p in probs]
     
     def pull(self, action):
+        """Pull the lever of the bandit
+        Parameters
+        ----------
+        action : int
+            the bandit id to pull
+        """
         return self.bandits[action].pull()
